86670de2149cbe6a90d033d317266dcd7dd62cba
Canberk Aykanat <kayra.aykanat@gmail.com>
2025-05-05 19:49:35 +0300

    Open API Configurations
    Open API Configurations

--- order-service/order-service/src/main/java/com/canberk/microservices/order/OrderServiceApplication.java
+++ order-service/order-service/src/main/java/com/canberk/microservices/order/OrderServiceApplication.java
@@ -2,8 +2,10 @@ package com.canberk.microservices.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "client")
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
--- order-service/order-service/src/main/java/com/canberk/microservices/order/service/OrderService.java
+++ order-service/order-service/src/main/java/com/canberk/microservices/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.canberk.microservices.order.service;
 
+import client.InventoryClient;
 import com.canberk.microservices.order.dto.OrderRequest.OrderRequest;
 import com.canberk.microservices.order.model.Order;
 import com.canberk.microservices.order.repository.OrderRepository;
@@ -13,15 +14,25 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
+
     public void placeOrder(OrderRequest orderRequest){
 
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setCode(orderRequest.code());
-        order.setQuantity(orderRequest.quantity());
+        var isProductInStock = inventoryClient.isInStock(orderRequest.code(), orderRequest.quantity());
 
-        orderRepository.save(order);
+        if(isProductInStock){
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setCode(orderRequest.code());
+            order.setQuantity(orderRequest.quantity());
+
+            orderRepository.save(order);
+        }
+        else{
+            throw new RuntimeException("Product with code " + orderRequest.code()+" is not in stock");
+        }
+
 
     }
 }
